2f30d2c729e4d671d02c437fd74e215159d70416
Elijah Sullivan <elijahsullivan@users.noreply.github.com>
2022-07-20 17:32:05 -0500

    refactor: rename contracts

--- src/Colloquium.sol
+++ src/Colloquium.sol
@@ -1,4 +1,27 @@
 // SPDX-License-Identifier: MIT 
 pragma solidity ^0.8.13;
 
-contract Colloquium {}
+import "solmate/tokens/ERC721.sol";
+
+contract Colloquium is ERC721 {
+    event Mint(address indexed minter, uint256 indexed tokenId, string indexed indexedContentId, string contentId);
+
+    constructor() ERC721("Colloquium", "COL") {}
+
+    mapping(uint256 => string) public contentIdentifierByTokenId;
+
+    function tokenURI(uint256 id) public view override returns (string memory) {
+        return string(abi.encodePacked("https://ipfs.io/ipfs/", contentIdentifierByTokenId[id]));
+    }
+
+    // might change this to mint to msg.sender (need to change frontend abi if so)
+    // using (address to) here rather than msg.sender could result in a denial of
+    // service attack, albeit costly, where a bad actor mints CIDs to the zero address
+    function mint(address to, string calldata cid) public {
+        uint256 id = uint256(keccak256(abi.encode(cid)));
+        // doing this costs a lot of gas.. maybe use the graph
+        contentIdentifierByTokenId[id] = cid;
+        _safeMint(to, id);
+        emit Mint(to, id, cid, cid);
+    }
+}
--- test/Colloquium.t.sol
+++ test/Colloquium.t.sol
@@ -3,10 +3,62 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
 
-contract ColloquiumTest is Test {
-    function setUp() public {}
+import "src/Colloquium.sol";
 
-    function testExample() public {
-        assertTrue(true);
+contract ColloquiumTest is Test {
+    event Mint(address indexed minter, uint256 indexed tokenId, string indexed indexedContentId, string contentId);
+
+    Colloquium internal colloquium;
+
+    function setUp() public {
+        colloquium = new Colloquium();
+    }
+
+    function testTokenHasName() public {
+        assertEq(colloquium.name(), "Colloquium");
+    }
+
+    function testTokenHasSymbol() public {
+        assertEq(colloquium.symbol(), "COL");
+    }
+
+    function testTokenHasUri() public {
+        string memory cid = "QmcRD4wkPPi6dig81r5sLj9Zm1gDCL4zgpEj9CfuRrGbzF";
+        uint256 tokenId = uint256(keccak256(abi.encode(cid)));
+
+        vm.prank(address(2));
+        colloquium.mint(address(1), cid);
+
+        assertEq(colloquium.tokenURI(tokenId), "https://ipfs.io/ipfs/QmcRD4wkPPi6dig81r5sLj9Zm1gDCL4zgpEj9CfuRrGbzF");
+    }
+
+    function testTokenIsMintable() public {
+        string memory cid = "QmcRD4wkPPi6dig81r5sLj9Zm1gDCL4zgpEj9CfuRrGbzF";
+        uint256 tokenId = uint256(keccak256(abi.encode(cid)));
+
+        vm.prank(address(2));
+        colloquium.mint(address(1), cid);
+        assertEq(colloquium.balanceOf(address(1)), 1);
+        assertEq(colloquium.ownerOf(tokenId), address(1));
+    }
+
+    function testTokenStoresContentIdentifier() public {
+        string memory cid = "QmcRD4wkPPi6dig81r5sLj9Zm1gDCL4zgpEj9CfuRrGbzF";
+        uint256 tokenId = uint256(keccak256(abi.encode(cid)));
+
+        vm.prank(address(2));
+        colloquium.mint(address(1), cid);
+        assertEq(colloquium.contentIdentifierByTokenId(tokenId), cid);
+    }
+
+    function testMintEmitsEvent() public {
+        string memory cid = "QmcRD4wkPPi6dig81r5sLj9Zm1gDCL4zgpEj9CfuRrGbzF";
+        uint256 tokenId = uint256(keccak256(abi.encode(cid)));
+
+        // need to change this from address 1 to address 2 if I change mint to msg.sender
+        vm.expectEmit(true, true, true, false); // how does this even work?
+        emit Mint(address(1), tokenId, cid, cid);
+        vm.prank(address(2));
+        colloquium.mint(address(1), cid);
     }
 }
